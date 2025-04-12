dc7efb0fe444df1077c71c1d5729b46e068087c0
Sahil Joshi <150469754+SahilJoshi121@users.noreply.github.com>
2025-04-12 13:38:56 -0700

    Initial Commit

---
+++ SnapCycle/SnapCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCycleApp.swift
+//  SnapCycle
+//
+//  Created by Sahil Joshi on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
