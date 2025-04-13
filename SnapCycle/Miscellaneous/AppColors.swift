0e33cf8ccd4867747fc92ac8d8bab1ebd586b6b3
Sahil Joshi <150469754+SahilJoshi121@users.noreply.github.com>
2025-04-12 21:40:11 -0700

    Added sapling assets and created SaplingPreviewView

---
+++ SnapCycle/Miscellaneous/AppColors.swift
@@ -0,0 +1,18 @@
+//
+//  AppColors.swift
+//  SnapCycle
+//
+//  Created by Sahil Joshi on 4/12/25.
+//
+
+import SwiftUI
+
+struct AppColors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppColors()
+}
