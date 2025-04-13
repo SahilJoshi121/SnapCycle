dd0ec24a4fda685736b9b9d30877488dbdcdddf0
Sahil Joshi <150469754+SahilJoshi121@users.noreply.github.com>
2025-04-12 22:41:15 -0700

    Finished SaplingView and Sapling logic

---
+++ SnapCycle/Views/SaplingView.swift
@@ -0,0 +1,18 @@
+//
+//  SaplingView.swift
+//  SnapCycle
+//
+//  Created by Sahil Joshi on 4/12/25.
+//
+
+import SwiftUI
+
+struct SaplingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SaplingView()
+}
---
+++ SnapCycle/Views/SubViews/GrowSaplingView.swift
@@ -0,0 +1,18 @@
+//
+//  GrowSaplingView.swift
+//  SnapCycle
+//
+//  Created by Sahil Joshi on 4/12/25.
+//
+
+import SwiftUI
+
+struct GrowSaplingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GrowSaplingView()
+}
